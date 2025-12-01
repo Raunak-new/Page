ac70341e5874d26a43d9ef2d48166961d8baff96
Raunak <raunakrauniyar14@gmail.com>
2025-12-01 20:31:53 +0545

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function About() {
+	return (
+	<div>
+		<h1>
+	About ME !!
+		</h1>
+		<h3>
+	My name is Raunak Rauniyar.
+		</h3>
+	</div>
+);
+}
+
+export default About;
---
+++ src/webpages/Projects.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div>
+      <ul>
+        <li>
+          Color Picker
+        </li>
+        <li>
+          Digital Clock
+        </li>
+        <li>
+          Stop Watch
+        </li>
+        <li>
+          To Do List
+        </li>
+        <li>
+          Counting App
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
